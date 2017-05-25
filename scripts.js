4bb12f16be11263472a2fceb7d2b286890a90b17
Joe Beltrami <jpbeltrami@gmail.com>
2017-05-24 17:34:11 -0700

    equals, clear, correct, updated

--- scripts.js
+++ scripts.js
@@ -8,44 +8,72 @@
 // Ans is the cumulative result of operations so far.
 // Ans has a button of its own to show the cumulative result so far.
 // if you enter a second input that is a number it automatically multiplies
-// all input is in a string and displayed in the input section until = pressed
+// all input is in a string and expressioned in the input section until = pressed
 // AC automatically becomes CE once input is entered
 // once = is pressed the result goes in the input area. the operation goes in the cumulative area
 
 $(document).ready(function(){
-  var display = "";
+  var expression = "";
   var keyInput;
   var ans;
 
-// number keys
+// check whether number key has been pressed before. if so the just pressing =
   $(".number").click(function(){
-    keyInput = $(this).attr('id');
-    console.log(keyInput);
-    // console.log(digit);
-    display += keyInput;
-    $("#dispAns").html(display);
+    keyInput = $(this).html();
+    expression += keyInput;
+    $("#dispAns").html(expression);
   });
 
   $(".operator").click(function(){
     keyInput = $(this).html();
-    console.log(keyInput);
-    // console.log(digit);
-    display += keyInput;
-    $("#dispAns").html(display);
+    expression += keyInput;
+    $("#dispAns").html(expression);
   });
 
   $("#equals").click(function(){
-    ans = eval(display);
-    console.log(ans);
-    $("#expression").html(ans);
+    ans = expression.split("");
+    ans = ans.filter(function(value){
+      return value != "=";
+    });
+
+    ans = ans.join("");
+    ans = eval(ans);
+
+    if(isNaN(ans)){
+      ans = "Error";
+      $("#expression").html(expression);
+      $("#dispAns").html(ans);
+      expression = "";
+      ans = 0;
+    }
+
+    else{
+      console.log(ans);
+      $("#expression").html(expression);
+      $("#dispAns").html(ans);
+      expression = "";
+    }
   });
 
-  // clear button
   $("#clear").click(function(){
     $("#dispAns").html("0");
     $("#expression").html("0");
+    expression = "";
+    ans = 0;
   });
 
-  // if equals button cliked then clear the field.
+  $("#correct").click(function(){
+    expression = expression.slice(0, -1);
+    $("#dispAns").html(expression);
+  });
+
+  $("#ans").click(function(){
+    if(typeof ans === "undefined"){
+      ans = 0;
+      expression = "Ans = ";
+      $("#dispAns").html(ans);
+      $("#expression").html(expression);
+    }
+  });
 
 });
