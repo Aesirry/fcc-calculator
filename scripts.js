ba816e5eeb6de640a39f2bfd087a1ece1f0d1ffc
Joe Beltrami <jpbeltrami@gmail.com>
2017-05-29 16:32:20 +0800

    dot cannot be inputted more than once

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-// can perform math between two numbers
-// can clear the input field with a clear button
-// can keep chaining mathematical operations together until I hit the equal button
-
-// HOW THE GOOGLE CALCULATOR WORKS
-// there is 0 in input when page first loaded.
-// there is an Ans variable where the cumulative result of previous operations is.
-// Ans is the cumulative result of operations so far.
-// Ans has a button of its own to show the cumulative result so far.
-// if you enter a second input that is a number it automatically multiplies
-// all input is in a string and expressioned in the input section until = pressed
-// AC automatically becomes CE once input is entered
-// once = is pressed the result goes in the input area. the operation goes in the cumulative area
-
 $(document).ready(function(){
   var expression = "";
   var keyInput;
@@ -19,6 +5,10 @@ $(document).ready(function(){
 
   $(".number").click(function(){
     keyInput = $(this).html();
+    // checks if a dot has already been inputted
+    if(keyInput == "." && expression.indexOf(".") != -1){
+      return;
+    }
     expression += keyInput;
     $("#dispAns").html(expression);
   }); // number
